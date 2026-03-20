8ea1b7f483f18274b618c969e2196497350ef4af
Araigoso <61702376+Araigoso@users.noreply.github.com>
2026-03-19 23:05:33 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const productos = [
+  {
+    nombre: "Remera blanca",
+    precio: 5000,
+    talle: "M",
+    img: "img/remera.jpg",
+    vendido: false
+  }
+];
+
+const contenedor = document.getElementById("productos");
+
+productos.forEach(p => {
+  const div = document.createElement("div");
+  div.className = "card";
+
+  div.innerHTML = `
+    <img src="${p.img}">
+    <h3>${p.nombre}</h3>
+    <p>Talle: ${p.talle}</p>
+    <p>$${p.precio}</p>
+    ${
+      p.vendido
+        ? "<span>VENDIDO</span>"
+        : `<a href="https://wa.me/549XXXXXXXXX?text=Quiero ${p.nombre}">Comprar</a>`
+    }
+  `;
+
+  contenedor.appendChild(div);
+});
