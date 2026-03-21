5599b550e4f9fddf198864a5d32de5fcc4d0acbd
Araigoso <61702376+Araigoso@users.noreply.github.com>
2026-03-20 21:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const productos = [
   {
-    nombre: "Buzo Manga Larga Brillo - Le utthe",
+    nombre: "Sweater Manga Larga Brillo - Le utthe",
     precio: 10000,
     talle: "M",
     img: "img/BuzoBrillo.png",
+    vendido: false
+  },
+  {
+    nombre: "Sweater Manga Larga Blanco con Perlas - Viaressa",
+    precio: 10000,
+    talle: "L",
+    img: "img/SweterBlanco.png",
+    vendido: false
+  },
+  
+   {
+    nombre: "Top Verde - ZARA",
+    precio: 8000,
+    talle: "L",
+    img: "img/RemeraZara.png",
     vendido: false
   },
   {
@@ -13,22 +28,65 @@ const productos = [
     img: "img/Conjunto.png",
     vendido: false
   },
+    {
+    nombre: "Short Deportivo - Urb Sport",
+    precio: 10000,
+    talle: "L",
+    img: "img/ShortDeportivo.png",
+    vendido: false
+  },
+      {
+    nombre: "Vestido Largo rojo",
+    precio: 10000,
+    talle: "L",
+    img: "img/VestidoLargoRojo.png",
+    vendido: false
+  },
+     {
+    nombre: "Remera Negra - AbsolutelyNot - Shein",
+    precio: 5000,
+    talle: "L",
+    img: "img/RemeraAbsolutelynot.png",
+    vendido: false
+  },
+       {
+    nombre: "Top Beige",
+    precio: 5000,
+    talle: "L",
+    img: "img/topBeige.png",
+    vendido: false
+  },
+    {
+    nombre: "Vestido Negro al Cuerpo",
+    precio: 8000,
+    talle: "M/L",
+    img: "img/VestidoNegroAlCuerpo.png",
+    vendido: false
+  },
   {
-    nombre: "Jean Beige",
+    nombre: "Jean Beige - Maitry",
     precio: 10000,
     talle: "42",
     img: "img/JeanBeige.png",
     vendido: false
   },
   {
-    nombre: "Jean Blanco",
-    precio: 10000,
+    nombre: "Jean Blanco - Maitry",
+    precio: 12000,
     talle: "42",
     img: "img/JeanBlanco.png",
     vendido: false
   },
+   {
+    nombre: "Short/Pollera Beige - Kill",
+    precio: 12000,
+    talle: "42",
+    img: "img/ShortKilii.png",
+    img2: "img/ShorKill.png",
+    vendido: false
+  },
   {
-    nombre: "Malla Amarilla - Nueva",
+    nombre: "Malla Amarilla - Le Blue - Nueva",
     precio: 15000,
     talle: "L",
     img: "img/Malla.png",
